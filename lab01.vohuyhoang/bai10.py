ac26076b1b4bb1bf8d68b80aac60116802665f8f
VoHuyHoangHenry <vohuyhoang2308@gmail.com>
2024-09-03 20:47:11 +0700

    Add files via upload

--- lab01.vohuyhoang/bai10.py
+++ lab01.vohuyhoang/bai10.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 30 13:49:55 2024
+Created on Tue Sep  3 20:37:00 2024
 
-@author: VoHuyHoang
+@author: ADMIN
 """
+
 so_xe = float(input("Nhập số xe gồm 4 chữ số: "))
 a = so_xe // 1000
 b = so_xe % 1000
@@ -13,8 +14,11 @@ e = d // 10
 f = d % 10
 tong = a + c + e + f
 
-if 1000 <= so_xe <= 9999:
+if  0 <= tong <= 9:
     print("Số nút là: ", tong)
 else:
-    print("Vui lòng nhập 4 chữ số") 
+    so_dau = tong // 10
+    so_cuoi = tong % 10
+    ket_qua = so_dau + so_cuoi
+    print("Số nút là: ", ket_qua)
               
